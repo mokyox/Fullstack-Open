0fcc7a4c0fa42b54ae36a1012eef6ff7bed8d067
Mo <mohamedjama53@gmail.com>
2019-09-05 13:20:16 +0100

    complete 1.4 exercise

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,18 +4,20 @@ import "./index.css";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
   const Header = props => {
     return (
@@ -38,9 +40,10 @@ const App = () => {
   const Content = props => {
     return (
       <React.Fragment>
-        <Part name={props.part1.name} exercises={props.part1.exercises}></Part>
-        <Part name={props.part2.name} exercises={props.part2.exercises}></Part>
-        <Part name={props.part3.name} exercises={props.part3.exercises}></Part>
+        {console.log(props)}
+        <Part name={props.parts[0].name} exercises={props.parts[0].exercises}></Part>
+        <Part name={props.parts[1].name} exercises={props.parts[1].exercises}></Part>
+        <Part name={props.parts[2].name} exercises={props.parts[2].exercises}></Part>
       </React.Fragment>
     );
   };
@@ -48,7 +51,10 @@ const App = () => {
   const Total = props => {
     return (
       <React.Fragment>
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        <p>
+          Number of exercises{" "}
+          {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        </p>
       </React.Fragment>
     );
   };
@@ -56,12 +62,8 @@ const App = () => {
   return (
     <React.Fragment>
       <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3}></Content>
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      ></Total>
+      <Content parts={parts}></Content>
+      <Total parts={parts}></Total>
     </React.Fragment>
   );
 };
