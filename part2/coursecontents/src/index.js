b82d81884980346ca488949a40766ecc07a7a40d
Mo <mohamedjama53@gmail.com>
2019-09-13 13:53:20 +0100

    complete exercise 2.4

--- part2/coursecontents/src/index.js
+++ part2/coursecontents/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const Course = ({ course }) => {
+  //Renders all courses
   return (
     <React.Fragment>
-      <Header name={course.name}></Header>
-      <Content parts={course.parts}></Content>
-      <Total parts={course.parts}></Total>
+      {course.map(course => {
+        return (
+          <React.Fragment key={course.id}>
+            <Header name={course.name}></Header>
+            <Content parts={course.parts}></Content>
+            <Total parts={course.parts}></Total>
+          </React.Fragment>
+        );
+      })}
     </React.Fragment>
   );
 };
@@ -53,35 +60,54 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <React.Fragment>
-      <Course course={course}></Course>
+      <Course course={courses}></Course>
     </React.Fragment>
   );
 };
