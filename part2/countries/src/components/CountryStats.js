5ff97b12d6c9301c7e3458671c6683af9acb122e
Mo <mohamedjama53@gmail.com>
2019-10-19 13:53:10 +0100

    revamp design using styled-components

--- part2/countries/src/components/CountryStats.js
+++ part2/countries/src/components/CountryStats.js
@@ -1,22 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const CountryStats = ({ country }) => {
   //Display stats for a country
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <CountryCard>
       <h1>{country.name}</h1>
       <p>Population: {new Intl.NumberFormat().format(country.population)}</p>
-      <h3>Languages</h3>
+      <h4>Languages</h4>
       <ul>
         {country.languages.map(language => (
           <li key={language.name}>{language.name}</li>
         ))}
       </ul>
       <img src={country.flag} alt="flag"></img>
-    </div>
+    </CountryCard>
   );
 };
+
+const CountryCard = styled.div`
+  /* border: solid 1px red; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 CountryStats.propTypes = {
   country: PropTypes.object
--- part2/countries/src/components/Results.js
+++ part2/countries/src/components/Results.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CountryStats from "./CountryStats";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const Results = ({ filteredCountries }) => {
   const [selectedCountry, setSelectedCountry] = useState("");
@@ -10,28 +11,65 @@ const Results = ({ filteredCountries }) => {
     return <p>Too many matches, please specify another filter.</p>;
   }
   return (
-    <ul>
-      {filteredCountries.map(country => (
-        <React.Fragment key={country.name}>
-          <li>{country.name}</li>
-          <button
-            onClick={() => {
-              setSelectedCountry(country);
-              setIsClicked(!isClicked);
-            }}
-          >
-            {selectedCountry.name === country.name && isClicked ? "Showing" : "Show"}
-          </button>
-
-          {/*TODO: Move <CountryStats></CountryStats> outside of the <li></li> so it doesn't render between them*/}
-        </React.Fragment>
-      ))}
+    <>
+      <StyledResults>
+        <ul>
+          {filteredCountries.map(country => (
+            <React.Fragment key={country.name}>
+              <StyledResult>
+                <li>{country.name}</li>
+                <StyledButton
+                  onClick={() => {
+                    setSelectedCountry(country);
+                    setIsClicked(!isClicked);
+                  }}
+                >
+                  Display
+                </StyledButton>
+              </StyledResult>
+            </React.Fragment>
+          ))}
+        </ul>
+      </StyledResults>
       {selectedCountry ? <CountryStats country={selectedCountry}></CountryStats> : ""}
-    </ul>
+    </>
   );
 };
+
+const StyledResults = styled.div`
+  /* border: solid 1px green; */
+  text-align: center;
+  margin: 5px 0;
+`;
+
+const StyledResult = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: nowrap;
+`;
+
+const StyledButton = styled.button`
+  background: ${props => (props.primary ? "#25d366" : "#e76c67")};
+  border: ${props => (props.primary ? "1px solid #25d366" : "1px solid #e76c67")};
+  color: white;
+  padding: 0.3rem 0.5rem;
+  border-radius: 5px;
+  text-decoration: none;
+  font-weight: 700;
+  transition: all 0.1s ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:hover,
+  &:focus {
+    outline: none;
+  }
+`;
 
 Results.propTypes = {
   filteredCountries: PropTypes.array
 };
+
 export default Results;
