8c7b6e41455bfc1cbd434e5a35eab42044faad17
Mo <mohamedjama53@gmail.com>
2019-11-21 10:30:12 +0000

    complete exercise 2.17

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 const Persons = ({ persons, searchPersons }) => {
   /* Map over array of objects and display name */
@@ -6,16 +7,36 @@ const Persons = ({ persons, searchPersons }) => {
     searchPersons === ""
       ? persons
       : persons.filter(
-          person => person.name.toLowerCase().indexOf(searchPersons.toLowerCase()) !== -1
+          person =>
+            person.name.toLowerCase().indexOf(searchPersons.toLowerCase()) !==
+            -1
         );
 
   console.log(personsFilter);
+
+  //Remove a user on Click from the backend
+  //Use the axios.delete() method onClick
+  // Remove the user depending on the id of the person.id
+
+  const removeUser = (id, person) => {
+    if (window.confirm(`Delete ${person}?`)) {
+      axios.delete(`http://localhost:3001/persons/${id}`);
+      console.log("Deleted.");
+    }
+  };
 
   return (
     <>
       {personsFilter.map(person => (
         <p key={person.name}>
           {person.name} {person.number}
+          <button
+            onClick={() => {
+              removeUser(person.id, person.name);
+            }}
+          >
+            Delete
+          </button>
         </p>
       ))}
     </>
